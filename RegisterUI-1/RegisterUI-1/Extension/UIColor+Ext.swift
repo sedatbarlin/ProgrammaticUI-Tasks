bed501352b30d99448f3560659cd5fe1fb6b724c
Sedat Barlin <barlinsedat@gmail.com>
2025-10-16 22:13:30 +0300

    Add UIColor extension with custom color properties
    Introduces UIColor+Ext.swift, providing static color properties for main and secondary UI elements. Updates the Xcode project to include the new extension in the build.

---
+++ RegisterUI-1/RegisterUI-1/Extension/UIColor+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Ext.swift
+//  RegisterUI-1
+//
+//  Created by Sedat on 16.10.2025.
+//
+
+import UIKit
+ 
+extension UIColor {
+    static var clrMainBlack = UIColor(red: 0.26, green: 0.26, blue: 0.26, alpha: 1.00)
+    static var clrSubtitle = UIColor(red: 0.74, green: 0.74, blue: 0.74, alpha: 1.00)
+    static var clrSecondaryBlack = UIColor(red: 0.38, green: 0.38, blue: 0.38, alpha: 1.00)
+    static var clrSecondaryTitle = UIColor(red: 0.62, green: 0.62, blue: 0.62, alpha: 1.00)
+    static var clrMainButton = UIColor(red: 1.00, green: 0.51, blue: 0.51, alpha: 1.00)
+}
