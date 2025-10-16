eef567a88c727a27f4fea4b4569bb55639a16084
Sedat Barlin <barlinsedat@gmail.com>
2025-10-16 21:56:57 +0300

    Initialize RegisterUI-1 iOS project structure
    Set up initial Xcode project with AppDelegate, SceneDelegate, Info.plist, asset catalogs, launch screen storyboard, and a basic splash ViewController. This provides the foundation for further development of the RegisterUI-1 app.

---
+++ RegisterUI-1/RegisterUI-1/Scenes/Splash/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RegisterUI-1
+//
+//  Created by Sedat on 16.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
