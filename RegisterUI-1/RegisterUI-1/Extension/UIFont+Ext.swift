304a0e80cbdd62e981eca060b331cd46d8d23e37
Sedat Barlin <barlinsedat@gmail.com>
2025-10-16 22:04:43 +0300

    Add Rubik font support and UIFont extension
    Added Rubik font files (Bold, Medium, SemiBold, Light, Regular) to the project and registered them in Info.plist. Introduced UIFont+Ext.swift to provide convenience initializers for using Rubik fonts throughout the app. Updated the Xcode project to include the new resources and source file.

---
+++ RegisterUI-1/RegisterUI-1/Extension/UIFont+Ext.swift
@@ -0,0 +1,30 @@
+//
+//  UIFont+Ext.swift
+//  RegisterUI-1
+//
+//  Created by Sedat on 16.10.2025.
+//
+
+import UIKit
+
+extension UIFont {
+    class func rubikBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Bold", size: size) ?? UIFont.boldSystemFont(ofSize: size)
+    }
+    
+    class func rubikLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    class func rubikMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    class func rubikRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+    
+    class func rubikSemiBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Rubik-SemiBold", size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+}
