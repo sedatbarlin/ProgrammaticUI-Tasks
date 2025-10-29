c5c8f3a1778da0b9499569398d8a69fb98ae84fa
Sedat Barlin <barlinsedat@gmail.com>
2025-10-29 22:11:25 +0300

    Initialize Xcode project for Login & Register UI
    Set up a new iOS Xcode project with basic structure, including AppDelegate, SceneDelegate, Info.plist, asset catalogs, a launch screen storyboard, and a placeholder LoginVC controller. This establishes the foundation for a login and registration UI application.

---
+++ Login&RegisterUI-2/Login&RegisterUI-2/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Login&RegisterUI-2
+//
+//  Created by Sedat on 29.10.2025.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+}
---
+++ Login&RegisterUI-2/Login&RegisterUI-2/Scenes/Login/Controller/LoginVC.swift
@@ -0,0 +1,16 @@
+//
+//  LoginVC.swift
+//  Login&RegisterUI-2
+//
+//  Created by Sedat on 29.10.2025.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
