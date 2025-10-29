f68b2c33b5830a4b0dfa0fe44d2bfd49801694af
Sedat Barlin <barlinsedat@gmail.com>
2025-10-29 22:31:27 +0300

    Add app logo and white background to auth screens
    Introduced a logo image asset and displayed it at the top of both Login and Register screens. Added a new clrMainWhite color and set it as the background for these screens. Updated project structure and imported SnapKit for layout constraints.

--- Login&RegisterUI-2/Login&RegisterUI-2/Extension/UIColor+Ext.swift
+++ Login&RegisterUI-2/Login&RegisterUI-2/Extension/UIColor+Ext.swift
@@ -9,4 +9,5 @@ import UIKit
  
 extension UIColor {
     static var clrMainGreen = UIColor(red: 0.00, green: 0.27, blue: 0.26, alpha: 1.00)
+    static var clrMainWhite = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00)
 }
--- Login&RegisterUI-2/Login&RegisterUI-2/Scenes/Login/Controller/LoginVC.swift
+++ Login&RegisterUI-2/Login&RegisterUI-2/Scenes/Login/Controller/LoginVC.swift
@@ -6,11 +6,28 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginVC: UIViewController {
+    
+    private lazy var appLogoImageView: UIImageView = {
+        let iv = UIImageView(image: .logo)
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI(){
+        view.backgroundColor = .clrMainWhite
         
+        view.addSubview(appLogoImageView)
+        appLogoImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
     }
 }
