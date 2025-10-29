cfdcd22434d1677d8357da539637089cb57389da
Sedat Barlin <barlinsedat@gmail.com>
2025-10-29 22:21:12 +0300

    Add RegisterVC and UIColor extension
    Introduced RegisterVC as a new UIViewController for the registration scene and added a UIColor extension with a custom clrMainGreen color. Updated the Xcode project to include these new files.

---
+++ Login&RegisterUI-2/Login&RegisterUI-2/Extension/UIColor+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Ext.swift
+//  Login&RegisterUI-2
+//
+//  Created by Sedat on 29.10.2025.
+//
+
+import UIKit
+ 
+extension UIColor {
+    static var clrMainGreen = UIColor(red: 0.00, green: 0.27, blue: 0.26, alpha: 1.00)
+}
---
+++ Login&RegisterUI-2/Login&RegisterUI-2/Scenes/Register/Controller/RegisterVC.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterVC.swift
+//  Login&RegisterUI-2
+//
+//  Created by Sedat on 29.10.2025.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
